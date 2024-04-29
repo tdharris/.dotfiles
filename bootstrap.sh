5f14c9519bdbd2c202ef1a021b66fe1ec6f2ad15
tdharris <tylerdavidharris@gmail.com>
2024-04-29 12:41:42 -0600

    Update bootstrap with dynamic alias imports

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,49 +2,25 @@
 
 DOTFILES_DIR="$HOME/.dotfiles"
 
-function source_files {
+function source_aliases {
     local -r aliases_dir="$1"
-    local -r prefixes=(
-        "asdf"
-        "aws"
-        "bw"
-        "context"
-        "env"
-        "extract"
-        "gruntwork"
-        "gh"
-        "git"
-        "iac"
-        "file"
-        "k9s"
-        "kubectl"
-        "log"
-        "prompts"
-        "terraform"
-        "terragrunt"
-    )
 
-    # shellcheck disable=SC2068
-    for prefix in ${prefixes[@]}; do
-        local alias_file="${aliases_dir}/${prefix}_aliases"
-        if [[ -f "${alias_file}" ]]; then
+    for alias_file in "$aliases_dir"/*_aliases; do
+        if [[ -f "$alias_file" ]]; then
             # shellcheck disable=SC1090
-            source "${alias_file}"
+            source "$alias_file"
         fi
     done
 }
 
-source_files "$DOTFILES_DIR/bash"
+# Source public aliases
+source_aliases "$DOTFILES_DIR/bash"
 
-# Source any "_aliases" files in the git submodules
+# Source private aliases
 for git_submodule in personal wrk; do
     sm="$DOTFILES_DIR/$git_submodule/bash"
     if [[ -d "$sm" ]]; then
-        for alias_file in "$sm"/*_aliases; do
-            if [[ -f "$alias_file" ]]; then
-                source "$alias_file"
-            fi
-        done
+        source_aliases "$sm"
     fi
 done
 
